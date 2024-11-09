183cee6b545fa9f69d00f6c489ef178bc4cefa12
Sara McGhee <sarajmcghee@gmail.com>
2024-11-09 11:36:39 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ import About from './components/About';
 import Packages from './components/Packages';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
-import Checkout from './components/Checkout'; // Import Checkout page
+import Checkout from './components/Checkout';
 import { CartProvider } from './CartContext';
 import './App.css';
 
 function App() {
   return (
     <CartProvider>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}> {/* Add this line for local and production path compatibility */}
         <MenuBar />
         <div className="content-wrapper">
           <Routes>
             <Route path="/" element={
               <>
                 <Hero />
-                <Testimonials />
-                <Packages />
+                <Testimonials /> {/* Testimonials before About */}
                 <About />
+                <Packages />
               </>
             } />
-            <Route path="/checkout" element={<Checkout />} /> {/* Checkout page route */}
+            <Route path="/checkout" element={<Checkout />} />
           </Routes>
         </div>
         <Footer />
